5dd9b72b89c57e4c217ced2e57b39127904d0bfa
Maxim Tretyakov <max@tretyakov-ma.ru>
2018-01-28 17:25:43 +0500

    more samples and nmea parser

--- extract_nmea.sh
+++ extract_nmea.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep -aoE $'\$G[A-Z]+RMC[A-Z\.,*0-9]+\n\r' samples/d6pro.data
+grep -aoE $'\$G[A-Z]+RMC[A-Z\.,*0-9]+\n' samples/trendvision.data
--- parse.rb
+++ parse.rb
@@ -1,13 +1,27 @@
-DELIMITER = "\x00\x99\x20\x00".freeze
+require 'csv'
 
-out = File.open('1.txt', 'w')
+# ["$GNRMC", "061414.000", "A", "6046.6769", "N", "04619.2771",
+#  "E", "0.08", "201.75", "050118", nil, nil, "D*74"]
+CSV.open('samples/trendvision.nmea', 'r').each do |row|
+  timestamp = DateTime.strptime("#{row[9]} #{row[1]}", '%d%m%y %H%M%S.%L')
+  speed = (row[7].to_f * 1.852).round(2)
+  bb = row[8].to_f
 
-begin
-  IO.foreach('samples/datakam.data', DELIMITER) do |line|
-    line.strip.scan(/\$GNRMC.*;/).each { |data| out.puts(data) }
+  if row[3] && row[5]
+    lat_deg  = row[3][0..1]
+    lat_min  = row[3][2...-1]
+
+    lat = (lat_deg.to_f + (lat_min.to_f / 60.0)).round(6)
+    lat = -lat if row[4] == 'S'
+
+    lon_deg = row[5][0..2]
+    lon_min = row[5][3..-1]
+
+    lon = (lon_deg.to_f + (lon_min.to_f / 60.0)).round(6)
+    lon = -lon if row[6] == 'W'
+  else
+    lat, lon = [nil, nil]
   end
-ensure
-  out.close
-end
 
-# puts content.split("\x00\x50\x20\x00").size
+  puts [timestamp, bb, speed, lat, lon].map(&:to_s).to_s
+end
